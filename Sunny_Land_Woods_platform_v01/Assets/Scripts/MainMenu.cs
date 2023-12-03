3d3e051b9e93a329694ceb8944994ed01838c0d6
nguyenvanan221 <nguyenvanan2212002@gmail.com>
2023-12-03 16:58:25 +0700

    update menu

--- Sunny_Land_Woods_platform_v01/Assets/Scripts/MainMenu.cs
+++ Sunny_Land_Woods_platform_v01/Assets/Scripts/MainMenu.cs
@@ -10,6 +10,16 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadControls()
+    {
+        
+    }
+
     public void Quit()
     {
         Application.Quit();
--- Sunny_Land_Woods_platform_v01/Assets/Scripts/SceneChange.cs
+++ Sunny_Land_Woods_platform_v01/Assets/Scripts/SceneChange.cs
@@ -5,13 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-    [SerializeField] private string sceneName;
-
+    //public GameObject winnerMenu;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            SceneManager.LoadScene(sceneName);
+            //UnlockNewLevel();
+            //winnerMenu.SetActive(true);
+            SceneManager.LoadScene("WinnerMenu");
+
+        }
+    }
+
+    void UnlockNewLevel()
+    {
+        if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
+        {
+            PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1);
+            PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel", 1) + 1);
+            PlayerPrefs.Save();
         }
     }
 }
